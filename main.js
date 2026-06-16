a31f21ead6b6d5920041beef1d9679af2cf8dd63
Ericson <ericsonikaria17@gmail.com>
2026-06-16 11:06:16 -0500

    Add contacts page, font/color controls, and begin form validation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function displayDateTime() {
+function displayDateTime() 
+{
     const datetimeElement = document.getElementById('datetime-display');
     const currentDateTime = new Date();
     
@@ -11,3 +12,50 @@ displayDateTime();
 
 // Update every second
 setInterval(displayDateTime, 1000);
+
+function fontSizeChange() 
+{
+    const sizeInput = document.getElementById('font-size-input');
+
+    const texTarget = document.querySelector('main');
+
+    texTarget.style.fontSize = sizeInput.value + "px";
+}
+
+function backgroundColorChange()
+{
+    const colorInput = document.getElementById('background-color-input');
+
+    const sectorTarget = document.querySelector('body');
+
+    sectorTarget.style.backgroundColor = colorInput.value;
+}
+
+// validation requirements.
+// First name — alphabetic only, first letter capitalized
+// Last name — same rules
+// First and last name cannot be the same
+// Phone — format (ddd) ddd-dddd
+// Email — must contain @ and .
+// Gender — one must be selected
+// Comment — at least 10 characters
+
+
+function validateName(id)
+{
+    const userName = document.getElementById(id).value;
+    const regex = /^[A-Z][a-zA-Z]*$/;
+
+    return regex.test(userName);
+}
+
+function validateForm()
+{
+    validateName('first-name');
+    validateName('last-name');
+}
+
+function check()
+{
+    
+}
